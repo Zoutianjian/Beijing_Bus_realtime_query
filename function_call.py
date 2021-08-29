a6799fa687283d904ae1791e0d83cdeefea2d556
Zoutianjian <71216305+Zoutianjian@users.noreply.github.com>
2021-08-29 12:33:45 +0800

    Add files via upload

--- function_call.py
+++ function_call.py
@@ -31,6 +31,7 @@ def consult_Line_Station(Line_ID_Str):
     Station_Str2 = '&token=eyJhbGciOiJIUzI1NiIsIlR5cGUiOiJKd3QiLCJ0eXAiOiJKV1QifQ.eyJwYXNzd29yZCI6IjY0ODU5MTQzNSIsInVzZXJOYW1lIjoiYmpidXMiLCJleHAiOjE2MjQ1MDMxMDB9.l1a915d9OleMq_6YUjch004V0ZGNsXlqa6iGMB_m0i8'
     Station_Str2 = '&token=eyJhbGciOiJIUzI1NiIsIlR5cGUiOiJKd3QiLCJ0eXAiOiJKV1QifQ.eyJwYXNzd29yZCI6IjY0ODU5MTQzNSIsInVzZXJOYW1lIjoiYmpidXMiLCJleHAiOjE2MjcwOTkyMDB9.OQYkF6rC9jfgxoC5nXDjjv1nqDIv3KfXqol0ATdts9g'
     Station_Str2 = '&token=eyJhbGciOiJIUzI1NiIsIlR5cGUiOiJKd3QiLCJ0eXAiOiJKV1QifQ.eyJwYXNzd29yZCI6IjY0ODU5MTQzNSIsInVzZXJOYW1lIjoiYmpidXMiLCJleHAiOjE2MzAxMjA3MDJ9.RIWvu5qeD2iziXk3kOEYJeeRge8hH1OuwDwhGxjew7w'
+    Station_Str2 = '&token=eyJhbGciOiJIUzI1NiIsIlR5cGUiOiJKd3QiLCJ0eXAiOiJKV1QifQ.eyJwYXNzd29yZCI6IjY0ODU5MTQzNSIsInVzZXJOYW1lIjoiYmpidXMiLCJleHAiOjE2MzI3MTUyMDB9.KAznHQ57w7lAhMapHhCtkTnEYm2nMu4PZ02_qriDF44'
     Station_url_str = Station_Str1 + Line_ID_Str + Station_Str2
 
     try:
@@ -52,6 +53,7 @@ def consult_Bus(Line_ID_Str,Station_Str):
     Bus_Str2 = '&token=eyJhbGciOiJIUzI1NiIsIlR5cGUiOiJKd3QiLCJ0eXAiOiJKV1QifQ.eyJwYXNzd29yZCI6IjY0ODU5MTQzNSIsInVzZXJOYW1lIjoiYmpidXMiLCJleHAiOjE2MjQ1MDMxMDB9.l1a915d9OleMq_6YUjch004V0ZGNsXlqa6iGMB_m0i8'
     Bus_Str2 = '&token=eyJhbGciOiJIUzI1NiIsIlR5cGUiOiJKd3QiLCJ0eXAiOiJKV1QifQ.eyJwYXNzd29yZCI6IjY0ODU5MTQzNSIsInVzZXJOYW1lIjoiYmpidXMiLCJleHAiOjE2MjcwOTkyMDB9.OQYkF6rC9jfgxoC5nXDjjv1nqDIv3KfXqol0ATdts9g'
     Bus_Str2 = '&token=eyJhbGciOiJIUzI1NiIsIlR5cGUiOiJKd3QiLCJ0eXAiOiJKV1QifQ.eyJwYXNzd29yZCI6IjY0ODU5MTQzNSIsInVzZXJOYW1lIjoiYmpidXMiLCJleHAiOjE2MzAxMjA3MDJ9.RIWvu5qeD2iziXk3kOEYJeeRge8hH1OuwDwhGxjew7w'
+    Bus_Str2 = '&token=eyJhbGciOiJIUzI1NiIsIlR5cGUiOiJKd3QiLCJ0eXAiOiJKV1QifQ.eyJwYXNzd29yZCI6IjY0ODU5MTQzNSIsInVzZXJOYW1lIjoiYmpidXMiLCJleHAiOjE2MzI3MTUyMDB9.KAznHQ57w7lAhMapHhCtkTnEYm2nMu4PZ02_qriDF44' 
     Bus_url_str = Bus_Str1 + Bus_ID_Str + Bus_Str2
     try:
         response2 = req.get(Bus_url_str)
